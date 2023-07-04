9a2e4b4e09434515799926032fd3c0f11b21d18c
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-07-04 17:01:43 -0400

    Created file for Login page and added some code and comments

---
+++ app/src/main/java/com/example/workoutgenerator/ui/theme/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.workoutgenerator.ui.theme
+
+// This class will contain the background code for the login screen
+class LoginActivity {
+    // holds the name of the user logged in
+    private var name: String = ""
+
+    // Constructor
+    fun LoginActivit() {}
+
+    // get name
+    fun getName(): String { return name }
+
+    // Get name from the EditView and assign it to the "name" variable
+    fun setName() {
+
+    }
+
+
+}
