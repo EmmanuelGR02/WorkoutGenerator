3ff47cdaaf7082ec2ef2276699859a0ee37cb015
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-07-08 20:11:04 -0400

    trying google authentication implementation

--- app/src/main/java/com/example/workoutgenerator/MainActivity.kt
+++ app/src/main/java/com/example/workoutgenerator/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.example.workoutgenerator
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.workoutgenerator.ui.theme.WorkoutGeneratorTheme
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
+
+const val RC_SIGN_IN = 123
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        var button = findViewById<Button>(R.id.sign_in_button)
+
+        button.visibility = View.GONE
+        findViewById<Button>(R.id.tv_name).visibility = View.VISIBLE
+
+        button.setOnClickListener() {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            var button = findViewById<Button>(R.id.sign_in_button)
+            var tv_name = findViewById<TextView>(R.id.tv_name)
+            button.visibility = View.VISIBLE
+            tv_name.text = acct.displayName
+            tv_name.visibility = View.GONE
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        var button = findViewById<Button>(R.id.sign_in_button)
+        var tv_name = findViewById<TextView>(R.id.tv_name)
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            button.visibility = View.VISIBLE
+            tv_name.text = account.displayName
+            tv_name.visibility = View.GONE
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+
+            button.visibility = View.VISIBLE
+            tv_name.text = ""
+            tv_name.visibility = View.GONE
+        }
     }
 }
