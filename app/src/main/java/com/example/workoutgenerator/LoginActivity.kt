e7c5d33e485a86502ff41fe5d172513c7ff060d1
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-07-10 00:42:10 -0400

    LoginActivity.kt

--- app/src/main/java/com/example/workoutgenerator/LoginActivity.kt
+++ app/src/main/java/com/example/workoutgenerator/LoginActivity.kt
@@ -3,18 +3,27 @@ package com.example.workoutgenerator
 // This class will contain the background code for the login screen
 class LoginActivity {
     // holds the name of the user logged in
-    private var name: String = ""
+    private var username: String = ""
+    private var password: String = ""
 
     // Constructor
-    fun LoginActivit() {}
-
-    // get name
-   // fun getName(): String { return name }
-
-    // Get name from the EditView and assign it to the "name" variable
-    fun setName() {
-
+    constructor(username: String, password: String) {
+        this.username = username
+        this.password = password
     }
 
+    // Getters
+    fun getUsername(): String {
+        return username
+    }
+    fun getPassword(): String {
+        return password
+    }
+
+    // Checks if the given username and password are in the database
+    // returns true if yes, false otherwise
+    fun isLoginValid(): Boolean {
+        return true
+    }
 
 }
