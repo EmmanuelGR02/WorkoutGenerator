61cbf91959cce9eae582dcc001b6b66bde3b722c
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-07-20 15:32:03 -0400

    Working on individually looking for the username and password given in the sign in layout and handling the errors

--- app/src/main/java/com/example/workoutgenerator/LoginActivity.kt
+++ app/src/main/java/com/example/workoutgenerator/LoginActivity.kt
@@ -12,23 +12,51 @@ class LoginActivity(val username : String? = null, val password : String? = null
     // Checks if the given username and password are in the database
     // returns true if yes, false otherwise
     fun isLoginValid(msg : TextView, callback: (Boolean) -> Unit) {
-        val arr = ArrayList<String>()
+        isUsernameValid(username.toString()) { isValid ->
+            if (isValid) {
 
+            } else {
+                msg.text = "Invalid username"
+            }
+        }
+    }
+
+
+    private fun isUsernameValid(username: String, callback: (Boolean) -> Unit) {
         database.child("user info").addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
+                val arr = ArrayList<String>()
                 for (i in p0.children) {
-                    val username = i.child("username").value
-                    val pswd = i.child("password").value
-                    arr.add(pswd.toString())
+                    val user = i.child("username").value
+                    arr.add(user.toString())
                 }
-                val isLoginValid = arr.contains(password)
-                callback(isLoginValid)
+                val isUsernameValid = arr.contains(username)
+                callback(isUsernameValid)
             }
 
             override fun onCancelled(p0: DatabaseError) {
-                msg.text = "Invalid password"
                 callback(false)
             }
+        })
+    }
+
+    private fun isPasswordValid(password : String, callback : (Boolean) -> Unit) {
+        database.child("user info").child(username.toString()).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val arr = ArrayList<String>()
+
+                for (i in snapshot.children) {
+                    val pswd = i.child("password").value
+                    arr.add(pswd.toString())
+                }
+                val isPasswordValid = arr.contains(password)
+                callback(isPasswordValid)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(false)
+            }
+
         })
     }
 
