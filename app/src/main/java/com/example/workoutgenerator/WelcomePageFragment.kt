90bf227c60fee0bc1956056bb1bcc7e876708f08
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-08-19 15:00:17 -0400

    Removed old layouts and code that was not needed anymore since I switched to fragment navigation

--- app/src/main/java/com/example/workoutgenerator/WelcomePageFragment.kt
+++ app/src/main/java/com/example/workoutgenerator/WelcomePageFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.example.workoutgenerator.databinding.FragmentWelcomePageBinding
-import com.example.workoutgenerator.databinding.WelcomeLayoutBinding
 
 class WelcomePageFragment : Fragment() {
     private lateinit var binding: FragmentWelcomePageBinding
