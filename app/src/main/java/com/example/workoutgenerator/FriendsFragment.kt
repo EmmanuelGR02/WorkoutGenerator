de1c62681b2a235b129f464b159fbfab0177a156
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-08-23 15:27:40 -0400

    Displaying current users friends layout working. Need to fix alignments and add all of the friends data in their layout

--- app/src/main/java/com/example/workoutgenerator/FriendsFragment.kt
+++ app/src/main/java/com/example/workoutgenerator/FriendsFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.ScrollView
 import android.widget.TextView
 import com.example.workoutgenerator.databinding.FragmentFriendsBinding
 
@@ -18,10 +17,8 @@ class FriendsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentFriendsBinding.inflate(inflater, container, false)
         val view = binding.root
-
 
         val profileBtn = binding.profileBtn
         val workoutBtn = binding.workoutBtn
@@ -36,7 +33,6 @@ class FriendsFragment : Fragment() {
             (requireActivity() as MainActivity).navigateToFragment(fragment)
         }
 
-        val scrollView = binding.scrollView
         val friendContainer = binding.friendContainer
 
         val myFriendsList: ArrayList<String> = ArrayList()
@@ -46,31 +42,18 @@ class FriendsFragment : Fragment() {
             myFriendsList.addAll(friendsList)
 
             // Fetch and add friend layouts
-            fetchAndAddFriendLayouts(myFriendsList, friendContainer, scrollView)
+            fetchAndAddFriendLayouts(myFriendsList, friendContainer)
         }
 
         return view
     }
 
-//    fun createFriendLayout(imageResource: Int, friendName: String): View {
-//        val inflater = LayoutInflater.from(requireContext())
-//        val friendLayout = inflater.inflate(R.layout.friend_layout_item, null)
-//
-//        val friendAvatar = friendLayout.findViewById<ImageView>(R.id.friendAvatar)
-//        val friendNameTextView = friendLayout.findViewById<TextView>(R.id.friendName)
-//
-//        friendAvatar.setImageResource(imageResource)
-//        friendNameTextView.text = friendName
-//
-//        return friendLayout
-//    }
-
     private fun fetchAndAddFriendLayouts(
         friendsList: List<String>,
-        friendContainer: LinearLayout,
-        scrollView: ScrollView
+        friendContainer: LinearLayout
     ) {
-        friendContainer.removeAllViews() // Clear existing views
+        // Clear existing views outside the loop
+        friendContainer.removeAllViews()
 
         val inflater = LayoutInflater.from(requireContext())
 
@@ -79,7 +62,7 @@ class FriendsFragment : Fragment() {
                 val imageResource = R.drawable.main_logo // Replace with actual image resource
 
                 // Create a new instance of the friend layout
-                val friendLayout = inflater.inflate(R.layout.friend_layout_item, null)
+                val friendLayout = inflater.inflate(R.layout.friend_layout_item, friendContainer, false)
 
                 val friendAvatar = friendLayout.findViewById<ImageView>(R.id.friendAvatar)
                 val friendNameTextView = friendLayout.findViewById<TextView>(R.id.friendName)
@@ -91,8 +74,7 @@ class FriendsFragment : Fragment() {
                 friendContainer.addView(friendLayout)
             }
         }
-
-        // Add the container to the scroll view after all friend layouts have been added
-        scrollView.addView(friendContainer)
     }
 }
+
+
