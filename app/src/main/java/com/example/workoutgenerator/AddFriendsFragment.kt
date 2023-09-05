04c7dc10191a5e724dc26822afc909409e9a8cff
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-09-04 20:39:55 -0400

    Added edit text to search or add friends on fragment_add_friends.xml. Might make a new fragment to add friends if it is too difficult to display friends or users depending on the characters being typed

--- app/src/main/java/com/example/workoutgenerator/AddFriendsFragment.kt
+++ app/src/main/java/com/example/workoutgenerator/AddFriendsFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.workoutgenerator.databinding.FragmentAddFriendsBinding
