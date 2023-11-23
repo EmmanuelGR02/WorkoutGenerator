006c349960c4a957164b3189d178450abdc540b3
Emmanuel <e.gutierrezrivera02@gmail.com>
2023-11-23 16:32:38 -0500

    working on logic to return chest workouts

--- app/src/main/java/com/example/workoutgenerator/GenerateWorkout.kt
+++ app/src/main/java/com/example/workoutgenerator/GenerateWorkout.kt
@@ -175,7 +175,7 @@ class GenerateWorkout : Fragment() {
         } else if (workoutsSelected == 2 && duration == "MEDIUM") {
             numOfWorkouts = 3
         } else if (workoutsSelected == 3 && duration == "MEDIUM") {
-            numOfWorkouts = 2//u7u
+            numOfWorkouts = 2
         }
 
         if (duration == "LONG" && blueWorkoutBtn.size == 1) {
